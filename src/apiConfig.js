d8c7c1d42abb3b11f1fd526102074281abd31863
DemoEto <paintcillssz@gmail.com>
2024-02-15 15:45:04 +0700

    uncomment dev

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,8 +1,8 @@
 export const apiConfig = { 
-    // development: {
-    //     bearer_token : '1234567890',
-    //     API_URL : 'http://localhost:4000',
-    // },
+    development: {
+        bearer_token : '1234567890',
+        API_URL : 'http://localhost:4000',
+    },
     production: {
         bearer_token : '1234567890',
         API_URL : 'https://api.se-rmutl.net',
