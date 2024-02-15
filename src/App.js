c2e95270428bd19effa322e2179c5cb789eaa67f
DemoEto <paintcillssz@gmail.com>
2024-02-15 15:56:02 +0700

    mov file MovieServices.js to src for find env.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { DisplayBoard } from './components/DisplayBoard'
 //----------------
 import { Movies } from './components/Movies'
 import CreateMovie from './components/CreateMovie'
-import { getAllMovies, createMovie } from './services/MovieService'
+import { getAllMovies, createMovie } from './MovieService'
 
 function App() {
 
